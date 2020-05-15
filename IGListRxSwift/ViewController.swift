8bb746e3361772c732691320e61aea93c936eae9
ikjeong.MCC <ikjeong@mycreditchain.io>
2020-05-15 16:19:19 +0900

    Initial Commit

---
+++ IGListRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IGListRxSwift
+//
+//  Created by ikjeong.MCC on 2020/05/15.
+//  Copyright © 2020 ikjeong.MCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
